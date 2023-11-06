409191c509c0b09ae2abeb2a181fbe9109570a4b
alca0004 <alca0004@gmail.com>
2023-11-06 10:14:23 -0500

    Build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+import { GiLongAntennaeBug } from 'react-icons/gi';
+
+const NavBar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center '>
+      <Link href='/'>
+        <GiLongAntennaeBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
+
+//npm install react-icons@4.11.0
